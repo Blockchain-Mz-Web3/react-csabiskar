6e2aee45714d86291531c267124dd1cfefa06984
csabiskar <csabiskar@gmail.com>
2024-06-15 14:06:10 +0530

    incorrect

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -4,7 +4,7 @@ import Button from './Button';
 
 
 test('renders the button with the correct text', () => {
-  render(<Button>Click Me</Button>);
+  render(<Button>Click</Button>);
   const buttonElement = screen.getByText(/click me/i);
   expect(buttonElement).toBeInTheDocument();
 });
